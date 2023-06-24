5747c4c9b634eeec64c08eb0c2d908074791c371
Rose <rosetsapze@gmail.com>
2023-06-24 04:16:27 +0100

    frontend connected to backend

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,91 +1,56 @@
-import React from "react";
-import "./testimonials.css";
-import IMG from "../../assets/me - Copy.jpg";
-import AV1 from "../../assets/avat1.JPG";
-import AV2 from "../../assets/avat2.JPG";
-import AV3 from "../../assets/avat3.JPG";
-import AV4 from "../../assets/avat4.JPG";
-import AV5 from "../../assets/avat5.JPG";
-
-// import Swiper core and required modules
-import { Pagination, Navigation, Scrollbar, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import './testimonials.css'
+import { Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/css/pagination'
 
+const Testimonial = () => {
+  const[data, setData] = useState([]);
 
-export const Testimonial = () => {
-  const testimonials = [
-    {
-      id: 1,
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: IMG,
-      Cname: "Client 1",
-    },
-    {
-      id: 2,
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: AV1,
-      Cname: "Client 2",
-    },
-    {
-      id: 3,
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: AV2,
-      Cname: "Client 3",
-    },
-    {
-      id: 4,
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: AV3,
-      Cname: "Client 4",
-    },
-    {
-      id: 5,
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: AV5,
-      Cname: "Client 5",
-    },
-    {
-      id: 6,
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: AV4,
-      Cname: "Client 6",
-    },
-  ];
+  useEffect(() => {
+    axios
+      .get("https://cyan-viper-belt.cyclic.app/api/testimonial")
+      .then((response) => {
+        const formattedData = response.data.data.map((item) => ({
+          id: item._id,
+          avatar: item.avatar,
+          name: item.name,
+          review: item.review,
+        }));
+        setData(formattedData);
+      })
+      .catch((error) => {
+        console.log("error fetching portfolio data:", error);
+      });
+  }, []);
+
   return (
-    <section id="testimonial">
-      <h5>Review from client</h5>
-      <h2>Testimonials</h2>
-
-      <Swiper className="container testimonials__container"
-      modules={[Pagination, Navigation, Scrollbar, A11y]}
+    <section id="testimonials">
+    <h5>Review from Clients</h5>
+    <h2>Testimonials</h2>
+    <Swiper
+      className="container testimonials__container"
+      modules={[Pagination]}
       spaceBetween={40}
       slidesPerView={1}
       pagination={{ clickable: true }}
-      >
-        {testimonials.map((testi) => {
-          return (
-            <SwiperSlide key={testi.id} className="testimonial">
-              <div className="client__avatar">
-                <img src={testi.avatar} alt="client avatar" />
-              </div>
-              <h5 className="client__name"> {testi.Cname} </h5>
-              <small className="client__review"> {testi.testimonial} </small>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </section>
-  );
-};
-export default Testimonial;
+    >
+      {
+        data.map(({ id, avatar, name, review }) => (
+        
+        <SwiperSlide key={id} className="testimonial">
+          <div className="client_avatar">
+            <img src={avatar} alt={name} />
+          </div>
+          <h5>{name}</h5>
+          <small className="client_review">{review}</small>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  </section>
+  )
+}
+
+export default Testimonial
